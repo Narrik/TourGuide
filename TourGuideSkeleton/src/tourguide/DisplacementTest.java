979a861e27fea7e03a01a24d824c19f498031262
s1684168 <s1684168@dotto.inf.ed.ac.uk>
2017-11-25 15:24:31 +0000

    Added distance and eastbearing tests.

--- TourGuideSkeleton/src/tourguide/DisplacementTest.java
+++ TourGuideSkeleton/src/tourguide/DisplacementTest.java
@@ -32,9 +32,15 @@ public class DisplacementTest {
     
     @Test
     public void testDistance() {
-        double bearing = new Displacement(0.0, 1.0).bearing();
-        assertEquals(0.0, bearing, EPS);
+        double distance1 = new Displacement(0.0, 1.0).distance();
+        double distance2 = new Displacement(4.0, -3.0).distance();
+        double distance3 = new Displacement(-3.0, 4.0).distance();
+        assertEquals(1.0, distance1, EPS);
+        assertEquals(5.0, distance2, EPS);
+        assertEquals(5.0, distance3, EPS);
     }
+    
+    
     
     
 }
