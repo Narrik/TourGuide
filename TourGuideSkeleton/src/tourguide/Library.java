55b8a5c81504534d1d9f9de19e0615efa9de7b4a
l1kh1 <22575057+likhithasai@users.noreply.github.com>
2017-11-25 04:40:59 +0000

    Implemented addTour

--- TourGuideSkeleton/src/tourguide/Library.java
+++ TourGuideSkeleton/src/tourguide/Library.java
@@ -1,7 +1,8 @@
 package tourguide;
-
+import java.util.*;
 public class Library {
+	HashMap<String,Tour> tour_lib = new HashMap<String,Tour>();
 	public void addTour(String id, String title, Annotation note, Tour tour){
-		
+		tour_lib.put(id,tour);
 	}
 }
