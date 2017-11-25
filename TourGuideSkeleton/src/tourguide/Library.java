edbba505343338bc5dc709cea808d6f3544d59cf
s1684168 <s1684168@dotto.inf.ed.ac.uk>
2017-11-25 16:21:43 +0000

    Changed tour_lib type to LinkedHashMap

--- TourGuideSkeleton/src/tourguide/Library.java
+++ TourGuideSkeleton/src/tourguide/Library.java
@@ -1,7 +1,7 @@
 package tourguide;
 import java.util.*;
 public class Library {
-	HashMap<String,Tour> tour_lib = new HashMap<String,Tour>();
+	LinkedHashMap<String,Tour> tour_lib = new LinkedHashMap<String,Tour>();
 	public void addTour(Tour tour){
 		tour_lib.put(tour.id,tour);
 	}
