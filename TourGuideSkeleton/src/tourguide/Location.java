2197ec7046cc87d71e836fb7a88fecae9b609a0e
l1kh1 <22575057+likhithasai@users.noreply.github.com>
2017-11-25 04:40:07 +0000

    Constructor and deltaFrom

--- TourGuideSkeleton/src/tourguide/Location.java
+++ TourGuideSkeleton/src/tourguide/Location.java
@@ -1,12 +1,17 @@
 package tourguide;
 
 public class Location {
-	public static double easting;
-	public static double northing;
-	
-	public Displacement deltaFrom(Location location) {
-		 
-		return null;
+	public double easting;
+	public double northing;
+	Location(double e, double n){
+		easting = e;
+		northing = n;
+	}
+	public Displacement deltaFrom(Location location, Location currWayp) {
+		easting = currWayp.easting - location.easting;
+		northing = currWayp.northing - location.northing;
+		Displacement disp = new Displacement(easting,northing);
+		return disp;
 		
 	}
 }
