0f22d5dfc69e8d73f6a60ead4fae48b78305ffc6
l1kh1 <22575057+likhithasai@users.noreply.github.com>
2017-11-25 04:48:16 +0000

    Constructor and near function

--- TourGuideSkeleton/src/tourguide/Waypoint.java
+++ TourGuideSkeleton/src/tourguide/Waypoint.java
@@ -2,9 +2,15 @@ package tourguide;
 
 public class Waypoint {
 	Annotation note;
-	
+	Location currWayp;
+	double radius; 
+	Waypoint(Annotation n, double r, Location loc){
+		currWayp = loc;
+		radius = r;
+		note = n;
+	}
 	public boolean near(Location location) {
-		return false;
+		return radius <= location.deltaFrom(location, currWayp).distance(); 
 	}
 	
 }
